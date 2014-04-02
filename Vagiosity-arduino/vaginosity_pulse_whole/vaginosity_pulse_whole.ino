bedd518ef8822db51a6516d00d1968baa2f6d559
Igor Chertenkov <dieworld@gmail.com>
2014-04-02 11:19:49 +0700

    fixed issues with repeated readings. now VAGI-VAGI checks for wave front and tail parity

--- Vagiosity-arduino/vaginosity_pulse_whole/vaginosity_pulse_whole.ino
+++ Vagiosity-arduino/vaginosity_pulse_whole/vaginosity_pulse_whole.ino
@@ -13,11 +13,20 @@ const int pitchPin = 5;
 const int pitchCal = 0;
 const int headingCal = -10;
 
-const int leftInPin = 7;
-const int rightInPin = 8;
+const int leftInPin = 8;
+const int rightInPin = 7;
 
-const int ONE = 1;
+const int ONE = 0;
 const int ZERO = 1 - ONE;
+
+
+const int buttonPin = 2;
+byte vagina[500];
+byte penis[500];
+int vaginaCounter = 0;
+int penisFlag = 0;
+
+
 
 const int MAX_TIME_BETWEEN_BYTES = 10000;
 
@@ -54,6 +63,10 @@ int oldLeft = 0, oldRight = 0;
 unsigned long lastBitReadTimestamp = 0;
 unsigned long inputNumber = 0;
 int inputDigits = 0;
+
+//every bit is read when wave front arrives. next read should be done only after 
+//wave tail has passed. this flag tells whether did we encounter wave tail or not yet
+boolean readyForNewBit = false;
 
 void applyData(unsigned long int data)
 {
@@ -124,8 +137,32 @@ void loop() {
   int left = digitalRead(leftInPin);
   int right = digitalRead(rightInPin);
   
-  if (left == ONE && oldLeft == ZERO) {
+  int buttonValue = digitalRead(buttonPin);
+  if (buttonValue) {
+    penisFlag = 1;
+    vaginaCounter = 0;
+  }
+  
+  if (penisFlag) {
+    if (vaginaCounter < 500) {
+      vagina[vaginaCounter] = left;
+      penis[vaginaCounter] = right;
+      vaginaCounter++;
+    }
+    
+    if (vaginaCounter == 500) {
+      for (int i = 0; i < 500; i ++) {
+        Serial.print(vagina[i]);
+        Serial.print(" ");
+        Serial.println(penis[i]);
+      }
+      vaginaCounter = 501;
+    }
+  }
+  
+  if (left == ONE && oldLeft == ZERO && readyForNewBit == true) { //wave front encountered
     unsigned long time = micros();
+    readyForNewBit = false;
     if (time - lastBitReadTimestamp > MAX_TIME_BETWEEN_BYTES) {
       inputNumber = 0;
       inputDigits = 0;
@@ -139,84 +176,12 @@ void loop() {
       Serial.print("number read: ");
       Serial.println(inputNumber, HEX);
       
-      applyData(inputNumber);
+      //applyData(inputNumber);
       
       inputDigits = 0;
       inputNumber = 0;
-  }
-  
-  
-  /*  if (mySwitch.available()) {
-    
-    unsigned long int data = mySwitch.getReceivedValue();
-    Serial.print(data, HEX);
-
-    int yValue = (data << 4 >> 25) * 8;
-    int xValue = (data << 11 >> 25) * 8;
-    int hValue = (data << 18 >> 25) * 8;
-    int pValue = (data << 25 >> 25) * 8;
-  */  
-/*    
-      float heading = (xValue - 512.0) / 1024.0;
-      float acc = - (yValue - 512.0) / 1024.0;
-      
-      float leftAcc = 0;
-      float rightAcc = 0;
-    
-      if (heading < -0.03) {
-        Serial.print(" -x<01 ");
-        leftAcc = acc;
-        rightAcc = acc * cos(heading * PI * 2);
-      }
-      if (heading > 0.03) {
-        Serial.print(" -x>01 ");
-        leftAcc = acc * cos(heading * PI * 2);
-        rightAcc = acc;
-      }
-      if (heading > -0.03 && heading < 0.03) {
-        Serial.print(" -xbtw ");
-        leftAcc = acc;
-        rightAcc = acc;
-      }
-      
-      leftAcc = 0;
-      rightAcc = 0;
-      
-      if (leftAcc < 0) {
-        analogWrite(leftMinusPin, 255.0 * leftAcc);
-        analogWrite(leftPlusPin, 0);
-      } 
-      if (leftAcc >= 0) {
-        analogWrite(leftPlusPin, 255.0 * leftAcc);
-        analogWrite(leftMinusPin, 0);
-      }
-      
-      if (rightAcc < 0) {
-        analogWrite(rightMinusPin, 255.0 * rightAcc);
-        analogWrite(rightPlusPin, 0);
-      }
-      if (rightAcc >= 0) {
-        analogWrite(rightPlusPin, 255.0 * rightAcc);
-        analogWrite(rightMinusPin, 0);
-      }
-      
-      Serial.print("head: "); Serial.print(heading);  
-      Serial.print(" acc: "); Serial.print(acc);  
-      Serial.print(" left: "); Serial.print(leftAcc);  
-      Serial.print(" right: "); Serial.println(rightAcc); 
-            
-      float pv = pitchCal + 90 + (hValue - 512) * 90.0 / 512.0;
-      float hv = headingCal + 90 + (pValue - 512) * 90.0 / 512.0;
-      if (hv < 0) hv = 0; if (pv < 0) pv = 0;
-      if (hv > 180) hv = 180; if (pv > 180) pv = 180;
-      pitchServo.write(pv);
-      headingServo.write(hv);
-
-      delay(10);
-
-  }
-  */
-  
-  
-  
+    }
+  } else if (left == ZERO && oldLeft == ONE && !readyForNewBit) { //wave tail encountered
+    readyForNewBit = true;
+  } 
 }
