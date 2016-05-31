2b9f4bf134024dac9eb9bd13d79020ae1e7d1419
Igor Chertenkov <dieworld@gmail.com>
2016-05-31 13:41:23 +0600

    arduino update

--- Vagiosity-arduino/vaginosity_manual_wheels/vaginosity_manual_wheels.ino
+++ Vagiosity-arduino/vaginosity_manual_wheels/vaginosity_manual_wheels.ino
@@ -4,11 +4,11 @@
 
 int leftPlusPin = 9;
 int leftMinusPin = 6;
-int rightPlusPin = 10;
-int rightMinusPin = 11;
+int rightPlusPin = 3;
+int rightMinusPin = 5;
 
-int xPin = A0;
-int yPin = A1;
+int xPin = A1;
+int yPin = A0;
 
 
 void setup() {
@@ -18,6 +18,9 @@ void setup() {
   pinMode(rightPlusPin, OUTPUT);  
   pinMode(rightMinusPin, OUTPUT);  
   
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  
   Serial.begin(9600);
 }
 
@@ -26,50 +29,49 @@ void loop() {
   float yValue = analogRead(yPin);
 
   float heading = (xValue - 512.0) / 1024.0;
-  float acc = (yValue - 512.0) / 1024.0;
+  float acc = (512.0 - yValue) / 1024.0;
   
   float leftAcc = 0;
   float rightAcc = 0;
 
   if (heading < -0.03) {
-    Serial.print("-x<01 ");
     leftAcc = acc;
     rightAcc = acc * cos(heading * PI * 2);
-  }
+  } else
   if (heading > 0.03) {
-    Serial.print("-x>01 ");
     leftAcc = acc * cos(heading * PI * 2);
     rightAcc = acc;
-  }
+  } else
   if (heading > -0.03 && heading < 0.03) {
-    Serial.print("-xbtw ");
     leftAcc = acc;
     rightAcc = acc;
   }
   
   if (leftAcc < 0) {
-    analogWrite(leftMinusPin, 255.0 * leftAcc);
+    analogWrite(leftMinusPin, 510.0 * (-leftAcc));
     analogWrite(leftPlusPin, 0);
-  } 
+  } else 
   if (leftAcc >= 0) {
-    analogWrite(leftPlusPin, 255.0 * leftAcc);
+    analogWrite(leftPlusPin, 510.0 * leftAcc);
     analogWrite(leftMinusPin, 0);
   }
   
   if (rightAcc < 0) {
-    analogWrite(rightMinusPin, 255.0 * rightAcc);
+    analogWrite(rightMinusPin, 510.0 * (-rightAcc));
     analogWrite(rightPlusPin, 0);
-  }
+  } else
   if (rightAcc >= 0) {
-    analogWrite(rightPlusPin, 255.0 * rightAcc);
+    analogWrite(rightPlusPin, 510.0 * rightAcc);
     analogWrite(rightMinusPin, 0);
   }
   
   delay(10);
 
-  Serial.print("head: "); Serial.print(heading);  
+  Serial.print("X: "); Serial.print(xValue);  
+  Serial.print(" Y: "); Serial.print(yValue);  
+  Serial.print(" head: "); Serial.print(heading);  
   Serial.print(" acc: "); Serial.print(acc);  
-  Serial.print(" left: "); Serial.print(leftAcc);  
-  Serial.print(" right: "); Serial.println(rightAcc);  
+  Serial.print(" left: "); Serial.print(512.0 * leftAcc);  
+  Serial.print(" right: "); Serial.println(512.0 * rightAcc);  
   
 }
